4d4adb1ea1fb4b11717208b359ffb18ae9b1945e
Blade Boles <bladerboles@gmail.com>
2020-01-27 10:41:11 -0700

    Started with bookmarks Router

--- src/bookmarks-router.js
+++ src/bookmarks-router.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const logger = require('./logger');
-
+const bookmarksRouter = express.Router();
 const bodyParser = express.json();
 const uuid = require('uuid/v4');
+const STORE  = require('./store');
 
+
+bookmarksRouter
+  .route('/bookmarks')
+  .get((req, res) => {
+    console.log(STORE);
+    res.json(STORE);
+  });
+
+bookmarksRouter
+  .route('/bookmarks/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    const bookmark = STORE.find(bm => bm.id === id);
+
+    if(!bookmark) {
+      logger.error(`Card with id ${id} was not found`);
+      return res
+        .status(404)
+        .send('Could not find that card');
+    }
+
+    res.json(bookmark);
+  });
+module.exports = bookmarksRouter;
---
+++ src/store.js
@@ -0,0 +1,11 @@
+/* eslint-disable quotes */
+
+const STORE = [{
+  "id": "8sdfbvbs65sd",
+  "title": "Google",
+  "url": "http://google.com",
+  "desc": "An indie search engine startup (lol)",
+  "rating": 4
+}];
+
+module.exports = STORE;
